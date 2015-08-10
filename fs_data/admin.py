4d311035e68829642e5ae7dd1b932987f0f71b4e
holgerd77 <Holger.Drewes@googlemail.com>
2015-08-10 16:29:49 +0200

    More comfortable admin overview

--- fs_data/admin.py
+++ fs_data/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 from .models import Country, Payment
 
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('recipient', 'country', 'zip_code', 'town', 'amount')
+
 
 admin.site.register(Country)
-admin.site.register(Payment)
+admin.site.register(Payment, PaymentAdmin)
 

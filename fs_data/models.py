972a3105163e8a586fbcca2b30cbb6cfdfbbbfcc
holgerd77 <Holger.Drewes@googlemail.com>
2016-01-12 10:54:03 +0100

    Draft of a simple flat data structure suitable for direct indexing

--- fs_data/models.py
+++ fs_data/models.py
@@ -3,22 +3,31 @@ from dynamic_scraper.models import Scraper
 from scrapy.contrib.djangoitem import DjangoItem
 
 
-class Country(models.Model):
-    code = models.CharField(max_length=2, primary_key=True)
+class Agency(models.Model):
+    country = models.CharField(max_length=2, primary_key=True)
     name = models.CharField(max_length=200)
-    public_body_url = models.URLField()
+    info_url = models.URLField()
+    scrape_url = models.URLField()
     scraper = models.ForeignKey(Scraper, blank=True, null=True, on_delete=models.SET_NULL)
+    comments = models.TextField(blank=True)
 
     def __unicode__(self):
-        return self.name
+        return '{name} (country)'.format(name=self.name, country=self.country)
 
 
 class Payment(models.Model):
-    recipient = models.CharField(max_length=200)
-    country = models.ForeignKey(Country)
-    zip_code = models.CharField(max_length=30)
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=30, blank=True)
     town = models.CharField(max_length=200)
-    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    region = models.CharField(max_length=200, blank=True)
+    year = models.IntegerField()
+    sub_payments = models.TextField(blank=True)
+    amount_nc = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
+    nc_symbol = models.CharField(max_length=3, blank=True)
+    nc_conv_date = models.DateTimeField(blank=True, null=True)
+    nc_conv_rate = models.DecimalField(max_digits=10, decimal_places=4)
+    amount_euro = models.DecimalField(max_digits=12, decimal_places=2)
 
     def __unicode__(self):
         return "%s [%s|%d]" % (self.recipient, self.country, self.amount)

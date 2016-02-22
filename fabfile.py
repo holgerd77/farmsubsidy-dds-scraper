feeaf2ebdcc7157fe1a0b2ddefbc256d45861de0
holgerd77 <Holger.Drewes@googlemail.com>
2016-02-22 12:04:16 +0100

    Added fabric task to server dependencies for deployment on AWS/EC2 (Ubuntu 14.04)

---
+++ fabfile.py
@@ -0,0 +1,25 @@
+from fabric.api import env, roles, run, sudo
+
+env.roledefs = {
+    'scraper': ['ubuntu@openfarmsubsidies.org'],
+}
+
+env.key_filename = '~/.ssh/open-farmsubsidies.pem'
+env.activate = 'source ~/venv-scraper/bin/activate'
+env.code_dir = '/home/ubuntu/farmsubsidy-dds-scraper/'
+
+@roles('scraper')
+def prepare_system():
+    sudo('apt-get update')
+    sudo('apt-get upgrade')
+
+@roles('scraper')
+def install_deps():
+    deps = [
+        'git',
+        'python3', 'python3-pip', 'python3-dev', 'python-venv',
+        'libxml2-dev', 'libffi-dev', 'libssl-dev', 'libxslt1-dev', 'libjpeg-dev',
+    ]
+    sudo('apt-get install ' + ' '.join(deps))
+    
+    
